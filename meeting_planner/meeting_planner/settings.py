190a395a4b24d77cb1eff64fb43002ceb5e89cac
Yullia Havrylenko <72810986+YuliiaHavrylenko@users.noreply.github.com>
2022-09-29 22:09:38 +0400

    Update settings.py

--- meeting_planner/meeting_planner/settings.py
+++ meeting_planner/meeting_planner/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$b#z!!+0uw3d87frq@)mk!49!vbdu*i(n$m87-e+!csvtc8b9b'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
