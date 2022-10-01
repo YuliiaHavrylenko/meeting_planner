f41d4665a363846672ff6da770e988df900ea8cb
Yuliia Havrylenko <volttabaki@gmail.com>
2022-10-01 23:54:37 +0400

    Add room model

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -3,6 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from datetime import datetime
 
 def welcome(request):
     return HttpResponse('Welcome To The Meeting Planner Application')
+
+
+def date(request):
+    return HttpResponse(f"This page was served at {str(datetime.now())}")
+
+def about(request):
+    return HttpResponse("My name is Volt")
